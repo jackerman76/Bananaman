8bc694d20712a4f066159b04be283d451a7e0120
Caleb Beichner <cmb286@pitt.edu>
2021-11-09 18:57:10 -0500

    Fix time

--- post.py
+++ post.py
@@ -20,7 +20,7 @@ class Post():
         self.status = status 
         
     def get_time(self):
-        return time.localtime(self.timestamp) - time.timezone
+        return time.localtime(self.timestamp)
 
     def get_time_as_time(self):
         return str(time.strftime("%H:%M %p", self.get_time()))
