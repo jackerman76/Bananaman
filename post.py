e881936e4a82bee99e2c85d605628a8a102b3572
Josh Ackerman <Joshackerman417@gmail.com>
2021-11-13 08:49:37 -0500

    think i fixed the time formatting stuff

--- post.py
+++ post.py
@@ -17,28 +17,42 @@ class Post():
         else:
             self.timestamp = time.time()
         self.picture = picture
-        self.status = status 
-        
-    def get_time(self):
-        return time.localtime(self.timestamp)
+        self.status = status
 
-    def get_time_as_time(self):
-        return str(time.strftime("%H:%M %p", self.get_time()))
+    #def get_time(self):
+        #return time.localtime(self.timestamp)
 
-    def get_time_as_date(self):
-        return str(time.strftime("%x", self.get_time()))
+    #def get_dt(self):
+    #    return datetime.fromtimestamp(self.timestamp)
 
-    def get_time_as_hrs_ago(self):
-        hrs = int(time.strftime("%H", time.localtime())) - int(time.strftime("%H", self.get_time()))
-        return str(hrs) + " hours ago"
+#    def get_time_as_time(self):
+#        return str(time.strftime("%H:%M %p", self.get_time()))
+
+#    def get_time_as_date(self):
+#        return str(time.strftime("%x", self.get_time()))
+
+#    def get_time_as_hrs_ago(self):
+#        hrs = int(time.strftime("%H", time.localtime())) - int(time.strftime("%H", self.get_time()))
+#        return str(hrs) + " hours ago"
+
+    #def get_formatted_time(self):
+        # calc difference in time in hours
+        #diff = (time.time() - self.timestamp) / (60 ** 2)
+
+    #    if (diff < 2):
+    #        return self.get_time_as_time()
+    #    if (diff < 24):
+    #        return self.get_time_as_hrs_ago()
+    #    else:
+    #        return self.get_time_as_date()
 
     def get_formatted_time(self):
-        # calc difference in time in hours
-        diff = (time.time() - self.timestamp) / (60 ** 2)
+        delta = datetime.now() - self.get_dt()
+        if delta.days != 0:
+            return str(days) + " days ago"
 
-        if (diff < 2):
-            return self.get_time_as_time()
-        if (diff < 24):
-            return self.get_time_as_hrs_ago()
+        if delta.seconds > 3600:
+            return str(delta.seconds/3600) + " hours ago"
+
         else:
-            return self.get_time_as_date()
+            return str(delta.seconds/60) + " minutes ago"
